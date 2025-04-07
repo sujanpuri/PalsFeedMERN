087a187d9c108e5e163c45ce2f912ef4bf755748
Sujan Puri <mrpurisujan@gmail.com>
2025-04-07 21:10:57 +0545

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [react(),tailwindcss()]
+})
